00218bbe9ae7a0b4f0ade49d519c93865421d5ba
Tehnikumi Opilane <opilane@tptlive.ee>
2019-05-02 11:48:06 +0300

    reworked view

--- components/ShowerThought.vue
+++ components/ShowerThought.vue
@@ -1,80 +1,66 @@
 <template>
-  <div class="container-fluid">
-    <section class="hero is-medium is-primary is-bold">
-      <div class="hero-body content_size">
-        <div class="  height_max">
-          <h1 class="title centerFlex height_max">
-            People think depressed people are lazy, when in actuality they are putting significantly more effort into
-            the
-            simple act of living than any content person
-          </h1>
+  <section class="hero is-success is-fullheight is-marginless">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head ">
+    </div>
 
-        </div>
-
-        <div class="columns spaceBetweenFlex">
-        <span class="icon is-large">
-          <span class="fa-stack fa-lg">
-            <i class="fas fa-chevron-left fa-stack-1x"></i>
-            <!--<i class="fas fa-ban fa-stack-2x has-text-danger"></i>-->
-          </span>
-        </span>
-          <span class="icon is-large">
-          <span class="fa-stack fa-lg">
-            <i class="far fa-star"></i>
-            <strong>2</strong>
-          </span>
-        </span>
-
-          <span class="icon is-large">
-          <span class="fa-stack fa-lg">
-            <i class="fas fa-chevron-right fa-stack-1x"></i>
-            <!--<i class="fas fa-ban fa-stack-2x has-text-danger"></i>-->
-          </span>
-        </span>
-        </div>
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body ">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          One day the last human will be born
+        </h1>
+        <h4 class="subtitle text-left">
+          Source
+        </h4>
       </div>
-    </section>
-  </div>
+    </div>
+
+    <!-- Hero footer: will stick at the bottom -->
+    <div class="hero-foot m-4b">
+      <div class="columns spaceBetweenFlex">
+          <span class="icon is-large is-size-4-mobile">
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-chevron-left fa-stack-1x"></i>
+              <i class="fas fa-ban fa-stack-2x has-text-danger"></i>
+            </span>
+          </span>
+        <span class="icon is-large is-size-4-mobile">
+
+              <strong>2000</strong>
+
+          </span>
+
+        <span class="icon is-large is-size-4-mobile">
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-chevron-right fa-stack-1x"></i>
+              <i class="fas fa-ban fa-stack-2x has-text-danger"></i>
+            </span>
+          </span>
+      </div>
+    </div>
+  </section>
 </template>
 
 
 <script>
-    export default {
-        name: "ShowerThought"
-    }
+  export default {
+    name: "ShowerThought"
+  }
 </script>
 
 <style scoped>
-  section {
 
-    height: calc(100vh - 3.25rem);
-  }
-
-  .content_size {
-    height: 100%;
-  }
-
-  .height_max {
-    height: 100%;
-  }
-
-  .centerFlex {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
 
   .spaceBetweenFlex {
     display: flex;
     align-items: center;
     justify-content: space-around;
   }
-
-  .flex_right_bottom {
-    height: 100%;
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-end;
+  .m-4b{
+    margin-bottom: 4rem;
   }
+
+
 
 </style>
